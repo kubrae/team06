2c84fd80104e499f6c2ab43a5acb384c90d1cab1
Kubra Ozdemir <kbrasylr345@gmail.com>
2023-05-12 11:04:23 +0300

    E

---
+++ src/Gitdeneme/deneme.java
@@ -0,0 +1,16 @@
+package Gitdeneme;
+
+public class deneme {
+
+    //  git init -->Local repo oluşturmak için yani git ile klasörümüzün içindeki dosyaları
+    // ilişkilendirmek için kullanılır
+
+    //  git status
+
+    //  git add .
+
+    // git commit -m
+
+    // git push
+   // ekleme yaptim
+}
